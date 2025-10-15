c9b3ca423b7cd5646cc2174a862f98c1ab36b8d3
sid-4998 <siddhantjha34@gmail.com>
2025-10-15 21:00:11 +0530

    Math functions

---
+++ intermediate/math.js
@@ -0,0 +1,51 @@
+console.log(Math.E)        // returns Euler's number
+console.log(Math.PI)       // returns PI
+console.log(Math.SQRT2)    // returns the square root of 2
+console.log(Math.SQRT1_2)  // returns the square root of 1/2
+console.log(Math.LN2)      // returns the natural logarithm of 2
+console.log(Math.LN10)     // returns the natural logarithm of 10
+console.log(Math.LOG2E)    // returns base 2 logarithm of E
+console.log(Math.LOG10E)   // returns base 10 logarithm of E
+
+// Methods
+console.log(Math.round(4.5));
+console.log(Math.round(4.6));
+console.log(Math.round(4.4));
+console.log(Math.round(-4.1));
+
+console.log(Math.ceil(4.1));
+console.log(Math.ceil(4.5));
+console.log(Math.ceil(4.6));
+console.log(Math.ceil(-4.2));
+
+console.log(Math.floor(4.4));
+console.log(Math.floor(4.5));
+console.log(Math.floor(4.6));
+console.log(Math.floor(-4.2));
+
+console.log(Math.trunc(4.4));
+console.log(Math.trunc(4.5));
+console.log(Math.trunc(4.6));
+console.log(Math.trunc(-4.2));
+
+console.log(Math.sign(4.6));
+console.log(Math.sign(-4.2));
+
+console.log(Math.pow(8,2));
+console.log(Math.sqrt(64));
+console.log(Math.abs(-4.7));
+console.log(Math.max(1,2,3));
+console.log(Math.min(1,2,3));
+
+console.log(Math.sin(90 * Math.PI/180));
+console.log(Math.cos(90 * Math.PI/180));
+
+// The angle must be in radians
+// Angle in radian = Angle in degree*PI/180
+
+console.log(Math.random()); // Returns a random number between 0(inclusive) and 1(exclusive)
+console.log(Math.log(1));   // returns the natural logarithm of 1
+console.log(Math.log2(2));  // returns the base 2 logarithm of 2
+console.log(Math.log10(10));  // returns the base 10 logarithm of 10
+
+// Refer to W3School for more Math functions
