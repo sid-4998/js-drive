99c7d3ee94b5c531cbb1bcf4b453b175f9246469
sid-4998 <siddhantjha34@gmail.com>
2025-11-06 10:37:51 +0530

    Object constructor functions

--- Advanced/objects.js
+++ Advanced/objects.js
@@ -56,3 +56,63 @@ const x = newPerson;
 x.age = 24;
 console.log(x);
 console.log(newPerson);
+
+// Object Constructors
+// When we have to create multiple objects of the same type
+// we use an object constructor function to create an object type
+// We can also add methods to an object constructor function
+
+function Person(first, last, age, eyeColor) {
+    this.firstname = first;
+    this.lastname = last;
+    this.age = age;
+    this.eyeColor = eyeColor;
+    this.nationality = 'Indian';
+    this.fullname = function () {
+        return this.firstname + " " + this.lastname;
+    }
+}
+// In a constructor function this has no value
+// Only when an object is created using the new keyword,
+// The value of this becomes equal to that object
+
+const myFather = new Person('Shailendra', 'Jha', 55, 'black');
+const myMother = new Person('Seema', 'Jha', 50, 'brown');
+const myBrother = new Person('Sarthak', 'Jha', 20, 'black');
+
+// Adding a property to an object
+// This property is only added to the myFather object.
+myFather.hobby = 'Reading';
+console.log(myFather);
+
+// When a default value is given to the object constructor,
+// it is applied to all the objects created from that constructor
+// The default value of nationality property is added to all objects
+console.log(myMother);
+console.log(myBrother);
+
+// We can also add a default value of a property directly to the 
+// object constructor function using the prototype keyword
+
+Person.prototype.city = 'Jamshedpur';
+console.log(myBrother.city);
+console.log(myMother.city);
+console.log(myFather.city);
+console.log(myFather.fullname());
+
+// We can also add a method to a created object
+myFather.changeEyeColor = function (color) {
+    this.eyeColor = color;
+}
+myFather.changeEyeColor('Brownish Black');
+console.log(myFather);
+
+// We can also directly add a method to object constructor function
+// using the prototype keyword
+
+Person.prototype.ChangeCity = function(city) {
+    this.city = city;
+}
+myBrother.ChangeCity('Pune');
+console.log(myBrother.city);
+console.log(myFather.city);
